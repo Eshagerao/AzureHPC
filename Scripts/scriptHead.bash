59bc50b5de014510f41ee0457c4d530b9504270a
Juan Jose Garcia Lopez <jjgarcia@solidq.com>
2016-02-11 23:17:58 +0100

    asd

---
+++ Scripts/scriptHead.bash
@@ -0,0 +1,7 @@
+#! /bin/bash
+# Las líneas que empiezan por "#" son comentarios
+# La primera línea o #! /bin/bash asegura que se interpreta como
+# un script de bash, aunque se ejecute desde otro shell.
+
+cd /home/userBBDD001/
+mkdir carpetaprueba2
