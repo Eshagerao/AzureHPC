517b71af8221603b8b584747242b281bed87fcc0
Juan Jose Garcia Lopez <jjgarcia@solidq.com>
2016-02-11 19:47:22 +0100

    scripts

---
+++ Scripts/scriptCompute.sh
@@ -0,0 +1,6 @@
+ #! /bin/bash
+ # Las líneas que empiezan por "#" son comentarios
+ # La primera línea o #! /bin/bash asegura que se interpreta como
+ # un script de bash, aunque se ejecute desde otro shell.
+
+mkdir carpetaprueba
