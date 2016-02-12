130e12907b0a36717465e90892d500644f834d94
Juan Jose Garcia Lopez <jjgarcia@solidq.com>
2016-02-12 13:32:59 +0100

    asdf

--- Scripts/scriptCompute.sh
+++ Scripts/scriptCompute.sh
@@ -2,6 +2,10 @@
 # Las líneas que empiezan por "#" son comentarios
 # La primera línea o #! /bin/bash asegura que se interpreta como
 # un script de bash, aunque se ejecute desde otro shell.
+var1=$5
+cd /home
 
-cd /home/userBBDD001/
-mkdir carpetaprueba
+for ((i=0;i<var1;i+=1));
+do
+echo "IaaSLnxCN-00$i" >> hosts.txt
+done
