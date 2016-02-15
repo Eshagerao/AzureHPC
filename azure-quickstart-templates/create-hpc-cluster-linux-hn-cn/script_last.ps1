34afeca0a63957de18d993d60e2c603bee1eafd8
Juan Jose Garcia Lopez <jjgarcia@solidq.com>
2016-02-15 16:00:28 +0100

    add azure deploy

---
+++ azure-quickstart-templates/create-hpc-cluster-linux-hn-cn/script_last.ps1
@@ -0,0 +1,5 @@
+﻿param([string]$rg = "rg-004", [string]$cnn = "2")
+$vmTemplate="C:\Users\Juanjo\Dropbox\SolidQ\1_TFM\Template\Last\azuredeployLast.json"
+
+New-AzureRmResourceGroup -Location "North Europe" -Name $rg
+New-AzureRmResourceGroupDeployment -ResourceGroupName $rg -TemplateUri $vmTemplate -computeNodeNumber $cnn
