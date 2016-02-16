a86e4f19c0edcccbb96819d139e45791600d0b68
Juan Jose Garcia Lopez <jjgarcia@solidq.com>
2016-02-16 13:47:55 +0100

    dfg

--- Scripts/scriptHead.sh
+++ Scripts/scriptHead.sh
@@ -12,6 +12,7 @@ echo "IaaSLnxCN-00$i" >> hosts
 done
 
 mkdir /home/userBBDD001/.ssh
+chmod 777 .ssh
 ssh-keygen -t rsa -N "" -f /home/userBBDD001/.ssh/id_rsa
 
 yum clean all
@@ -24,6 +25,7 @@ for ((i=0;i<var1;i+=1));
 do
 var2=$((var2 + 1))
 echo "/home           10.0.0.$var2(rw,sync,no_root_squash,no_subtree_check)" >> exports
+ssh-keyscan -H 10.0.0.$var2 >> /home/userBBDD001/.ssh/known_hosts
 done
 
 exportfs -a
