cfd78532f02537fe1d19151d9584b26fd08b2064
Juan Jose Garcia Lopez <jjgarcia@solidq.com>
2016-02-16 18:51:55 +0100

    add parameters script

--- azure-quickstart-templates/create-hpc-cluster-linux-hn-cn/Scripts/scriptHead.sh
+++ azure-quickstart-templates/create-hpc-cluster-linux-hn-cn/Scripts/scriptHead.sh
@@ -4,26 +4,30 @@
 # un script de bash, aunque se ejecute desde otro shell.
 var1=$1
 var2=4
+vmname=$2
+var0=0
+var00=00
+user=$3
 
 # Add hosts names
 cd /home
 for ((i=0;i<var1;i+=1));
 do
 if [ "$i" -lt 10 ] ; then
-    echo "IaaSLnxCN-00$i" >> hosts
+    echo "$vmname$var00$i" >> hosts
 else
         if [ "$i" -lt 100 ] ; then
-                echo "IaaSLnxCN-0$i" >> hosts
+                echo "$vmname$var0$i" >> hosts
         else
-                echo "IaaSLnxCN-$i" >> hosts
+                echo "$vmname$i" >> hosts
         fi
 fi
 done
 
 # Create dir .ssh and public ssh key
-mkdir /home/userBBDD001/.ssh
+mkdir /home/$user/.ssh
 chmod 777 .ssh
-ssh-keygen -t rsa -N "" -f /home/userBBDD001/.ssh/id_rsa
+ssh-keygen -t rsa -N "" -f /home/$user/.ssh/id_rsa
 
 # Install NFS server packages
 yum clean all
@@ -38,15 +42,15 @@ do
 var2=$((var2 + 1))
 echo "/home           10.0.0.$var2(rw,sync,no_root_squash,no_subtree_check)" >> exports
 if [ "$i" -lt 10 ] ; then
-    echo "10.0.0.$var2 IaaSLnxCN-00$i" >> hosts
+    echo "10.0.0.$var2 $vmname$var00$i" >> hosts
 else
         if [ "$i" -lt 100 ] ; then
-                echo "10.0.0.$var2 IaaSLnxCN-0$i" >> hosts
+                echo "10.0.0.$var2 $vmname$var0$i" >> hosts
         else
-                echo "10.0.0.$var2 IaaSLnxCN-$i" >> hosts
+                echo "10.0.0.$var2 $vmname$i" >> hosts
         fi
 fi
-ssh-keyscan -H 10.0.0.$var2 >> /home/userBBDD001/.ssh/known_hosts
+ssh-keyscan -H 10.0.0.$var2 >> /home/$user/.ssh/known_hosts
 done
 
 exportfs -a
