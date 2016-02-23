cea59ae1b34b1d59f47d376a560ec7a1e5894c52
Juan Jose Garcia Lopez <jjgarcia@solidq.com>
2016-02-23 18:33:06 +0100

    user route

--- azure-quickstart-templates/create-hpc-cluster-linux-hn-cn/Scripts/scriptCompute.sh
+++ azure-quickstart-templates/create-hpc-cluster-linux-hn-cn/Scripts/scriptCompute.sh
@@ -2,13 +2,20 @@
 # Las líneas que empiezan por "#" son comentarios
 # La primera línea o #! /bin/bash asegura que se interpreta como
 # un script de bash, aunque se ejecute desde otro shell.
-var1=5
-cd /home
+user=$1
 
-for ((i=0;i<var1;i+=1));
-do
-echo "IaaSLnxCN-00$i" >> hosts
-done
+cd /home/$user
+echo "-----------------------------------------------" >> info
+echo "Los nombres de los host se encuentran en" >> info
+echo "/mnt/nfs/home/hosts" >> info
+echo "-----------------------------------------------" >> info
+echo "Las claves privadas y publicas se encuentran en" >> info
+echo "/mnt/nfs/home/$user/.ssh/id_rsa" >> info
+echo "-----------------------------------------------" >> info
+
+# Disable firewall
+systemctl disable firewalld
+systemctl stop firewalld
 
 mkdir ~/.ssh
 ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa
