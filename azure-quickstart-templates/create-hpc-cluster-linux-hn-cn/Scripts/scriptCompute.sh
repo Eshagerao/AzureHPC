8b34f852af8affe620268cea8e34187b9b96bd98
Juan Jose Garcia Lopez <jjgarcia@solidq.com>
2016-05-19 16:54:49 +0200

    fix

--- azure-quickstart-templates/create-hpc-cluster-linux-hn-cn/Scripts/scriptCompute.sh
+++ azure-quickstart-templates/create-hpc-cluster-linux-hn-cn/Scripts/scriptCompute.sh
@@ -2,25 +2,25 @@
 # Las líneas que empiezan por "#" son comentarios
 # La primera línea o #! /bin/bash asegura que se interpreta como
 # un script de bash, aunque se ejecute desde otro shell.
-#user=$1
+user=$1
 
-#cd /home/$user
-#echo "-----------------------------------------------" >> info
-#echo "Los nombres de los host se encuentran en" >> info
-#echo "/mnt/nfs/home/hosts" >> info
-#echo "-----------------------------------------------" >> info
-#echo "Las claves privadas y publicas se encuentran en" >> info
-#echo "/mnt/nfs/home/$user/.ssh/id_rsa" >> info
-#echo "-----------------------------------------------" >> info
+cd /home/$user
+echo "-----------------------------------------------" >> info
+echo "Los nombres de los host se encuentran en" >> info
+echo "/mnt/nfs/home/hosts" >> info
+echo "-----------------------------------------------" >> info
+echo "Las claves privadas y publicas se encuentran en" >> info
+echo "/mnt/nfs/home/$user/.ssh/id_rsa" >> info
+echo "-----------------------------------------------" >> info
 
-# Disable firewall
-#systemctl disable firewalld
-#systemctl stop firewalld
+#Disable firewall
+systemctl disable firewalld
+systemctl stop firewalld
 
-#mkdir ~/.ssh
-#ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa
+mkdir ~/.ssh
+ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa
 
-#yum clean all
+yum clean all
 #yum install nfs-utils nfs-utils-lib -y
 
 #mkdir -p /mnt/nfs/home
