b9d74e1a0ce9973c01daba9a014f56b793f448c6
Juan Jose Garcia Lopez <jjgarcia@solidq.com>
2016-05-20 11:30:10 +0200

    yum head

--- azure-quickstart-templates/create-hpc-cluster-linux-hn-cn/Scripts/scriptHead.sh
+++ azure-quickstart-templates/create-hpc-cluster-linux-hn-cn/Scripts/scriptHead.sh
@@ -34,32 +34,32 @@ chmod 777 .ssh
 ssh-keygen -t rsa -N "" -f /home/$user/.ssh/id_rsa
 
 # Install NFS server packages
-yum clean all
-yum install nfs-utils nfs-utils-lib -y
-service rpcbind start
-service nfs start
+# yum clean all
+# yum install nfs-utils nfs-utils-lib -y
+# service rpcbind start
+# service nfs start
 
 # Add host to export NFS, IP and name to hosts file and host to known_host for SSH
-cd /etc
-for ((i=0;i<var1;i+=1));
-do
-var2=$((var2 + 1))
-echo "/home           10.0.0.$var2(rw,sync,no_root_squash,no_subtree_check)" >> exports
-if [ "$i" -lt 10 ] ; then
-    echo "10.0.0.$var2 $vmname$var00$i" >> hosts
-else
-        if [ "$i" -lt 100 ] ; then
-                echo "10.0.0.$var2 $vmname$var0$i" >> hosts
-        else
-                echo "10.0.0.$var2 $vmname$i" >> hosts
-        fi
-fi
-ssh-keyscan -H 10.0.0.$var2 >> /home/$user/.ssh/known_hosts
-done
+# cd /etc
+# for ((i=0;i<var1;i+=1));
+# do
+# var2=$((var2 + 1))
+# echo "/home           10.0.0.$var2(rw,sync,no_root_squash,no_subtree_check)" >> exports
+# if [ "$i" -lt 10 ] ; then
+#     echo "10.0.0.$var2 $vmname$var00$i" >> hosts
+# else
+#         if [ "$i" -lt 100 ] ; then
+#                 echo "10.0.0.$var2 $vmname$var0$i" >> hosts
+#         else
+#                 echo "10.0.0.$var2 $vmname$i" >> hosts
+#         fi
+# fi
+# ssh-keyscan -H 10.0.0.$var2 >> /home/$user/.ssh/known_hosts
+# done
 
-exportfs -a
+# exportfs -a
 
 # Finally install GCC (c++ and fortran) and OPEN_MPI
-yum install make gcc gcc-c++ gcc-gfortran -y
-yum -y install openmpi openmpi-devel -y
+# yum install make gcc gcc-c++ gcc-gfortran -y
+# yum -y install openmpi openmpi-devel -y
 
