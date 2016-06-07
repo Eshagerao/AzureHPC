979556d711ab6df12ba8c8f085c0dc857062adcd
Juan Jose Garcia Lopez <jjgarcia@solidq.com>
2016-06-07 23:46:37 +0200

    asdf

--- azure-quickstart-templates/create-hpc-cluster-linux-hn-cn-71-rdma/Scripts/scriptCompute.sh
+++ azure-quickstart-templates/create-hpc-cluster-linux-hn-cn-71-rdma/Scripts/scriptCompute.sh
@@ -13,21 +13,4 @@ echo "Las claves privadas y publicas se encuentran en" >> info
 echo "/mnt/nfs/home/$user/.ssh/id_rsa" >> info
 echo "-----------------------------------------------" >> info
 
-#Disable firewall
-systemctl disable firewalld
-systemctl stop firewalld
 
-# Create dir .ssh and public ssh key
-mkdir /home/$user/.ssh
-chmod 777 .ssh
-ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa
-
-#yum clean all
-#yum install deltarpm -y
-#yum update -y
-#yum update NetworkManager.x86_64 -y
-#yum update lvm2-7:2.02.105-14.el7.x86_64 -y
-# install nfs-utils -y
-
-#mkdir -p /mnt/nfs/home
-#mount 10.0.0.4:/home /mnt/nfs/home
