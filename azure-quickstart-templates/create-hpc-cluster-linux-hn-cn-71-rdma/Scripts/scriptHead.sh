e1c0f94d932260f0cd5f5a84904137908e85e1ad
Juan Jose Garcia Lopez <jjgarcia@solidq.com>
2016-06-22 19:45:09 +0200

    sdgf

--- azure-quickstart-templates/create-hpc-cluster-linux-hn-cn-71-rdma/Scripts/scriptHead.sh
+++ azure-quickstart-templates/create-hpc-cluster-linux-hn-cn-71-rdma/Scripts/scriptHead.sh
@@ -57,3 +57,41 @@ systemctl start rpcbind
 systemctl start nfs-server
 systemctl start nfs-lock
 systemctl start nfs-idmap
+
+# Add host to export NFS, IP and name to hosts file and host to known_host for SSH
+echo "##Add host to export NFS, IP and name to hosts file and host to known_host for SSH" >> /home/logg
+var2=4
+for ((i=0;i<var1;i+=1));
+do
+var2=$((var2 + 1))
+echo "/home           10.0.0.$var2(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
+if [ "$i" -lt 10 ] ; then
+    echo "10.0.0.$var2 $vmname$var00$i" >> /etc/hosts
+else
+         if [ "$i" -lt 100 ] ; then
+                 echo "10.0.0.$var2 $vmname$var0$i" >> /etc/hosts
+         else
+                 echo "10.0.0.$var2 $vmname$i" >> /etc/hosts
+         fi
+fi
+#ssh-keyscan -H 10.0.0.$var2 >> /home/$user/.ssh/known_hosts
+# known_hosts para que sean del usuario
+echo "##known_hosts para que sean del usuario" >> /home/logg
+khst1='ssh-keyscan -H 10.0.0.'
+khst2=$khst1$var2
+khst1="$khst2 >> /home/"
+khst2=$khst1$user
+khstf="$khst2/.ssh/known_hosts"
+echo $khstf >> /home/logg
+cd /home/$user
+#su - $user -c 'ssh-keyscan -H 10.0.0."$var2" >> ~/.ssh/known_hosts'
+su - $user -c '$khstf'
+done
+
+# Permisos de la carpeta /home/usuario/.ssh
+chown -R $user:$user /home/$user/.ssh
+
+#Start the NFS service
+systemctl restart nfs-server 
+
+echo "##Fin del script" >> /home/logg
