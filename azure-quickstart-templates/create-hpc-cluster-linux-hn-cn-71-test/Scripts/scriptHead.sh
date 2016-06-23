1b3f022e192241509cd7b49b3a04d418f8f2de11
Juan Jose Garcia Lopez <jjgarcia@solidq.com>
2016-06-23 23:59:16 +0200

    adf

--- azure-quickstart-templates/create-hpc-cluster-linux-hn-cn-71-test/Scripts/scriptHead.sh
+++ azure-quickstart-templates/create-hpc-cluster-linux-hn-cn-71-test/Scripts/scriptHead.sh
@@ -17,12 +17,15 @@ systemctl stop firewalld
 echo "Create dir .ssh and public ssh key" >> /home/logg
 mkdir /home/user01/.ssh
 chmod 777 .ssh
-su - $user -c 'ssh-keygen -t rsa -N "" -f /home/user01/.ssh/id_rsa'
+sudo -u user01 ssh-keygen -t rsa -N "" -f /home/user01/.ssh/id_rsa
+#su - user01 -c 'ssh-keygen -t rsa -N "" -f /home/user01/.ssh/id_rsa'
 cd /home/user01
 echo "##known_hosts para que sean del usuario" >> /home/logg
 #sudo -u user01 ssh-keyscan -H 10.0.0.5 >> /home/$user/.ssh/known_hosts
 #sudo -u user01 ssh-keyscan -H 10.0.0.6 >> /home/$user/.ssh/known_hosts
-su - user01 -c 'ssh-keyscan -H 10.0.0.5 >> /home/user01/.ssh/known_hosts'
-su - user01 -c 'ssh-keyscan -H 10.0.0.6 >> /home/user01/.ssh/known_hosts'
+sudo -u user01 ssh-keyscan -H 10.0.0.5 >> /home/user01/.ssh/known_hosts
+sudo -u user01 ssh-keyscan -H 10.0.0.6 >> /home/user01/.ssh/known_hosts
+#su - user01 -c 'ssh-keyscan -H 10.0.0.5 >> /home/user01/.ssh/known_hosts'
+#su - user01 -c 'ssh-keyscan -H 10.0.0.6 >> /home/user01/.ssh/known_hosts'
 
 echo "##Fin del script test" >> /home/logg
