6deee48f0e057abb6fe6554d919689eafb695b12
Juan Jose Garcia Lopez <jjgarcia@solidq.com>
2016-06-23 17:27:55 +0200

    test

---
+++ azure-quickstart-templates/create-hpc-cluster-linux-hn-cn-71-test/Scripts/scriptCompute.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+# Las líneas que empiezan por "#" son comentarios
+# La primera línea o #! /bin/bash asegura que se interpreta como
+# un script de bash, aunque se ejecute desde otro shell.
+user=$1
+
+cd /home/$user
+echo "-----------------------------------------------" >> info
+echo "Los nombres de los host se encuentran en" >> info
+echo "/mnt/nfs/home/hosts" >> info
+echo "-----------------------------------------------" >> info
+echo "Las claves privadas y publicas se encuentran en" >> info
+echo "/mnt/nfs/home/$user/.ssh/id_rsa" >> info
+echo "-----------------------------------------------" >> info
+
+#Disable firewall
+systemctl disable firewalld
+systemctl stop firewalld
+
+mkdir ~/.ssh
+chmod 777 .ssh
+su - $user -c 'ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa'
+chown -R $user:$user /home/$user/.ssh
+
+yum clean all
+yum install deltarpm -y
+#yum update -y
+yum update NetworkManager.x86_64 -y
+yum install nfs-utils -y
+
+mkdir -p /home
+mount 10.0.0.4:/home /home
+
+#mkdir -p /mnt/nfs/home
+#mount 10.0.0.4:/home /mnt/nfs/home
---
+++ azure-quickstart-templates/create-hpc-cluster-linux-hn-cn-71-test/Scripts/scriptHead.sh
@@ -0,0 +1,109 @@
+#! /bin/bash
+# Las líneas que empiezan por "#" son comentarios
+# La primera línea o #! /bin/bash asegura que se interpreta como
+# un script de bash, aunque se ejecute desde otro shell.
+var1=$1
+var2=4
+vmname=$2
+var0=0
+var00=00
+user=$3
+idrsa1='ssh-keygen -t rsa -N "" -f /home/'
+idrsa2=$idrsa1$user
+idrsaf="$idrsa2/.ssh/id_rsa"
+
+# Disable firewall
+systemctl disable firewalld
+systemctl stop firewalld
+
+# Create dir .ssh and public ssh key
+echo "Create dir .ssh and public ssh key" >> /home/logg
+mkdir ~/.ssh
+chmod 777 .ssh
+su - $user -c 'ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa'
+cd /home/$user
+echo "##known_hosts para que sean del usuario" >> /home/logg
+sudo -u user01 ssh-keyscan -H 10.0.0.5 >> ~/.ssh/known_hosts
+sudo -u user01 ssh-keyscan -H 10.0.0.6 >> ~/.ssh/known_hosts
+
+
+# Add hosts names
+cd /home
+for ((i=0;i<var1;i+=1));
+do
+var2=$((var2 + 1))
+if [ "$i" -lt 10 ] ; then
+    echo "10.0.0.$var2 $vmname$var00$i" >> hosts
+else
+        if [ "$i" -lt 100 ] ; then
+                echo "10.0.0.$var2 $vmname$var0$i" >> hosts
+        else
+                echo "10.0.0.$var2 $vmname$i" >> hosts
+        fi
+fi
+done
+
+# Install NFS server packages
+echo "##Install NFS server packages" >> /home/logg
+yum clean all
+yum install deltarpm -y
+
+# We need to update some packages to solve some problems
+echo "##We need to update some packages to solve some problems" >> /home/logg
+#yum update -y
+yum update NetworkManager.x86_64 -y
+yum install nfs-utils -y
+
+# Next we need to start the services and add them to the boot menu.
+echo "##Next we need to start the services and add them to the boot menu." >> /home/logg
+systemctl enable rpcbind
+systemctl enable nfs-server
+systemctl start rpcbind
+systemctl start nfs-server
+systemctl start nfs-lock
+systemctl start nfs-idmap
+
+# Add host to export NFS, IP and name to hosts file and host to known_host for SSH
+echo "##Add host to export NFS, IP and name to hosts file and host to known_host for SSH" >> /home/logg
+var2=4
+for ((i=0;i<var1;i+=1));
+do
+var2=$((var2 + 1))
+echo "/home           10.0.0.$var2(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
+if [ "$i" -lt 10 ] ; then
+    echo "10.0.0.$var2 $vmname$var00$i" >> /etc/hosts
+else
+         if [ "$i" -lt 100 ] ; then
+                 echo "10.0.0.$var2 $vmname$var0$i" >> /etc/hosts
+         else
+                 echo "10.0.0.$var2 $vmname$i" >> /etc/hosts
+         fi
+fi
+#ssh-keyscan -H 10.0.0.$var2 >> /home/$user/.ssh/known_hosts
+# known_hosts para que sean del usuario
+#khst1='ssh-keyscan -H 10.0.0.'
+#khst2=$khst1$var2
+#khst1="$khst2 >> /home/"
+#khst2=$khst1$user
+#khstf="$khst2/.ssh/known_hosts"
+#echo $khstf >> /home/logg
+#cd /home/$user
+#su - $user -c 'ssh-keyscan -H 10.0.0."$var2" >> ~/.ssh/known_hosts'
+#su - $user -c '$khstf'
+#cd /home/$user
+#sudo -u user01 ssh-keyscan -H 10.0.0.5 >> ~/.ssh/known_hosts
+#sudo -u user01 ssh-keyscan -H 10.0.0.6 >> ~/.ssh/known_hosts
+done
+
+#cd /home/$user
+#wget https://github.com/JuanJoseGarciaUCLM/AzureHPC/raw/master/azure-quickstart-templates/Scripts/knowHost.sh
+#su - $user -c 'sh knowHost.sh $var1 $vmname $user'
+
+
+# Permisos de la carpeta /home/usuario/.ssh
+chown -R $user:$user /home/$user/.ssh
+
+#Start the NFS service
+systemctl restart nfs-server 
+
+echo "##Fin del script" >> /home/logg
