7d44f6eb8e7abc371f2cb54a7bde7b7cb7e3c244
Juan Jose Garcia Lopez <jjgarcia@solidq.com>
2016-06-24 17:33:52 +0200

    adsf

--- azure-quickstart-templates/create-hpc-cluster-linux-hn-cn-71-test/Scripts/scriptCompute.sh
+++ azure-quickstart-templates/create-hpc-cluster-linux-hn-cn-71-test/Scripts/scriptCompute.sh
@@ -2,15 +2,15 @@
 # Las líneas que empiezan por "#" son comentarios
 # La primera línea o #! /bin/bash asegura que se interpreta como
 # un script de bash, aunque se ejecute desde otro shell.
-user=$1
+usuario=$1
 
-cd /home/$user
+cd /home/$usuario
 echo "-----------------------------------------------" >> info
 echo "Los nombres de los host se encuentran en" >> info
 echo "/mnt/nfs/home/hosts" >> info
 echo "-----------------------------------------------" >> info
 echo "Las claves privadas y publicas se encuentran en" >> info
-echo "/mnt/nfs/home/$user/.ssh/id_rsa" >> info
+echo "/mnt/nfs/home/$usuario/.ssh/id_rsa" >> info
 echo "-----------------------------------------------" >> info
 
 #Disable firewall
@@ -19,5 +19,5 @@ systemctl stop firewalld
 
 mkdir ~/.ssh
 chmod 777 .ssh
-su - $user -c 'ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa'
-chown -R $user:$user /home/$user/.ssh
+su - $usuario -c 'ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa'
+chown -R $usuario:$usuario /home/$usuario/.ssh
