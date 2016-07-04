d54b44c008104eda3a30c68185b1e0552ef53520
Juan Jose Garcia Lopez <jjgarcia@solidq.com>
2016-07-04 19:58:01 +0200

    asdf

--- azure-quickstart-templates/create-hpc-cluster-linux-hn-cn-71-open/script_powershell.ps1
+++ azure-quickstart-templates/create-hpc-cluster-linux-hn-cn-71-open/script_powershell.ps1
@@ -1,7 +1,6 @@
-﻿param([string]$rg = "rg-001", [string]$cnn = "2")
+﻿param([string]$rg = "rg-001", [string]$cnn = "2", [string]$cname = "2")
 $vmTemplate="https://raw.githubusercontent.com/JuanJoseGarciaUCLM/AzureHPC/master/azure-quickstart-templates/create-hpc-cluster-linux-hn-cn-71-open/azuredeploy.json"
 cd "C:/Users/Juanjo/Dropbox/SolidQ/1_TFM/Template/github/azure-quickstart-templates/create-hpc-cluster-linux-hn-cn-71-open"
 Login-AzureRmAccount
-.\script_powershell.ps1 -rg rg-001 -name hpcc -cnn 2
 New-AzureRmResourceGroup -Location "North Europe" -Name $rg
-New-AzureRmResourceGroupDeployment -ResourceGroupName $rg -TemplateUri $vmTemplate -computeNodeNumber $cnn
+New-AzureRmResourceGroupDeployment -ResourceGroupName $rg -TemplateUri $vmTemplate -computeNodeNumber $cnn -clusterName $cname
