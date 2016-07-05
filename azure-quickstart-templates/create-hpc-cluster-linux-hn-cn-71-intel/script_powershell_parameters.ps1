a943b84e3363b1c932850fb9dd1df92ec889a487
Juan Jose Garcia Lopez <jjgarcia@solidq.com>
2016-07-05 21:06:41 +0200

    asdfasdfasdf

---
+++ azure-quickstart-templates/create-hpc-cluster-linux-hn-cn-71-intel/script_powershell_parameters.ps1
@@ -0,0 +1,6 @@
+﻿param([string]$rg = "rg-001")
+$vmTemplate="https://raw.githubusercontent.com/JuanJoseGarciaUCLM/AzureHPC/master/azure-quickstart-templates/create-hpc-cluster-linux-hn-cn-71-intel/azuredeploy.json"
+$vmParameter="https://raw.githubusercontent.com/JuanJoseGarciaUCLM/AzureHPC/master/azure-quickstart-templates/create-hpc-cluster-linux-hn-cn-71-intel/azuredeploy.parameters.json"
+Login-AzureRmAccount
+New-AzureRmResourceGroup -Location "North Europe" -Name $rg
+New-AzureRmResourceGroupDeployment -ResourceGroupName $rg -TemplateUri $vmTemplate -TemplateParameterUri $vmParameter
