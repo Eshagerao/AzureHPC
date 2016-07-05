539ba6a8669ea3221262ff54711d14a658b16d91
Juan Jose Garcia Lopez <jjgarcia@solidq.com>
2016-07-05 18:57:37 +0200

    asdfasdf

--- azure-quickstart-templates/create-hpc-cluster-linux-hn-cn-71-open/script_powershell_parameters.ps1
+++ azure-quickstart-templates/create-hpc-cluster-linux-hn-cn-71-open/script_powershell_parameters.ps1
@@ -1,4 +1,4 @@
-﻿param([string]$rg = "rg-003")
+﻿param([string]$rg = "rg-004")
 $vmTemplate="https://raw.githubusercontent.com/JuanJoseGarciaUCLM/AzureHPC/master/azure-quickstart-templates/create-hpc-cluster-linux-hn-cn-71-open/azuredeploy.json"
 $vmParameter="https://raw.githubusercontent.com/JuanJoseGarciaUCLM/AzureHPC/master/azure-quickstart-templates/create-hpc-cluster-linux-hn-cn-71-open/azuredeploy.parameters.json"
 Login-AzureRmAccount
