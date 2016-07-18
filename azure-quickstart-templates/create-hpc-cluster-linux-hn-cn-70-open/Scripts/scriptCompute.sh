0150b89a29b3a7507e7b28eca396f1b4d6632fca
Juan Jose Garcia Lopez <jjgarcia@solidq.com>
2016-07-18 23:10:27 +0200

    sdaf

--- azure-quickstart-templates/create-hpc-cluster-linux-hn-cn-70-open/Scripts/scriptCompute.sh
+++ azure-quickstart-templates/create-hpc-cluster-linux-hn-cn-70-open/Scripts/scriptCompute.sh
@@ -42,7 +42,7 @@ yum install epel-release netcdf-devel netcdf-fortran-devel netcdf-fortran-mpich-
 yum install -y perl perl-CPAN perl-CPAN-Meta
 yum install patch -y
 
-wget https://raw.githubusercontent.com/JuanJoseGarciaUCLM/AzureHPC/master/Scripts/perl.sh
+wget https://raw.githubusercontent.com/JuanJoseGarciaUCLM/AzureHPC/master/Scripts/perlcompute.sh
 chmod 777 perl.sh
 #curl -L http://cpanmin.us | perl - --self-upgrade
 #su - $usuario -c 'cpanm install App::perlbrew'
