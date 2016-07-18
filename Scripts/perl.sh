25dc5af303cca43385fd34205be8adab937eb35e
Juan Jose Garcia Lopez <jjgarcia@solidq.com>
2016-07-18 22:38:29 +0200

    asdfasd

---
+++ Scripts/perl.sh
@@ -0,0 +1,10 @@
+﻿#! /bin/bash
+# Las líneas que empiezan por "#" son comentarios
+# La primera línea o #! /bin/bash asegura que se interpreta como
+# un script de bash, aunque se ejecute desde otro shell.
+
+curl -L http://cpanmin.us | perl - --self-upgrade
+cpanm --sudo install App::perlbrew
+cpanm --sudo XML::LibXML
+
+setsebool -P use_nfs_home_dirs 1
