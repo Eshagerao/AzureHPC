36feb833191fdd0242e22125cb863681c4d4437d
Juan Jose Garcia Lopez <jjgarcia@solidq.com>
2016-07-19 18:18:25 +0200

    asdf

--- azure-quickstart-templates/create-hpc-cluster-linux-hn-cn-70-open/Scripts/scriptHead.sh
+++ azure-quickstart-templates/create-hpc-cluster-linux-hn-cn-70-open/Scripts/scriptHead.sh
@@ -90,13 +90,13 @@ exit
 curl -L http://cpanmin.us | sudo perl - --self-upgrade
 
 #curl -L http://cpanmin.us | sudo perl - --self-upgrade
-su - $usuario -c 'curl -L http://cpanmin.us | perl - --self-upgrade'
+#su - $usuario -c 'curl -L http://cpanmin.us | perl - --self-upgrade'
 
 #su - $usuario -c 'cpanm install App::perlbrew'
 #su - $usuario -c 'cpanm XML::LibXML'
 
-su -c "cpanm install App::perlbrew" $usuario
-su -c "cpanm XML::LibXML" $usuario
+#su -c "cpanm install App::perlbrew" $usuario
+#su -c "cpanm XML::LibXML" $usuario
 
 #cpanm --sudo install App::perlbrew
 #cpanm --sudo XML::LibXML
