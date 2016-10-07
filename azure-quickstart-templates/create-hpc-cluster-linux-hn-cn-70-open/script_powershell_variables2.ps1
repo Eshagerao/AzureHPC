797c5789b3eb42684d8c87bbb16475d882da4f8b
Juan Jose Garcia Lopez <jjgarcia@solidq.com>
2016-10-07 20:02:02 +0200

    AT LAST

--- azure-quickstart-templates/create-hpc-cluster-linux-hn-cn-70-open/script_powershell_variables2.ps1
+++ azure-quickstart-templates/create-hpc-cluster-linux-hn-cn-70-open/script_powershell_variables2.ps1
@@ -1,4 +1,4 @@
-﻿param([string]$rg = "rg-002", [string]$vmsize = "Standard_A1", [string]$cnn = "2", [string]$cname = "hpcsudodos")
+﻿param([string]$rg = "rg-001", [string]$vmsize = "Standard_A8", [string]$cnn = "7", [string]$cname = "hpc")
 $vmTemplate="https://raw.githubusercontent.com/JuanJoseGarciaUCLM/AzureHPC/master/azure-quickstart-templates/create-hpc-cluster-linux-hn-cn-70-open/azuredeploy.json"
 Login-AzureRmAccount
 New-AzureRmResourceGroup -Location "North Europe" -Name $rg
